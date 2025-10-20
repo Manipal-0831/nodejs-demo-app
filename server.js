4d350cd9e35ac3508e2f4e5e88c9721bae2a9516
Manipal <manipalreddy0831@gmail.com>
2025-10-20 19:54:00 +0530

    Fixed Server.js

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ app.get('/', (req, res) => {
   res.send('Node.js CI/CD Demo running successfully!');
 });
 
-app.listen(PORT, () => console.log(Server started on port ${PORT}));
+app.listen(PORT, () => console.log('Server started on port ${PORT}'));
