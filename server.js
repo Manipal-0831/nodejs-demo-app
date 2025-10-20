f2a2b977734a848551fc75a9170ab93ca7d54057
Manipal <manipalreddy0831@gmail.com>
2025-10-20 18:42:14 +0530

    Initial Node.js demo app

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Node.js CI/CD Demo running successfully!');
+});
+
+app.listen(PORT, () => console.log(Server started on port ${PORT}));
